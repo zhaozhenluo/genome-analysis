d07d0b6ebf59b2d9fd44220c61f09b6882a02f22
zhaozhenluo <60196701+zhaozhenluo@users.noreply.github.com>
2020-05-11 17:43:09 +0800

    lab11_3

--- app.R
+++ app.R
@@ -7,32 +7,16 @@
 #    http://shiny.rstudio.com/
 #
 
-
-
+library(shiny)
 
 library(shiny)
 library(tidyverse)
-library(ggplot2)
 library(lubridate)
 library(maps)
 library(mapdata)
 library(wesanderson)
-library(rsconnect)
 
-
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-  
-  output$distPlot <- renderPlot({
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-  })
-}
+# Exercises from Lab 11
 
 
 
@@ -49,73 +33,91 @@ time_series_covid19_deaths_global<-
   read_csv(url("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")) %>% 
   rename(Country_Region="Country/Region", Province_State= "Province/State")%>%
   pivot_longer(-c(Province_State, Country_Region, Lat, Long), names_to= "Date", values_to= "Death")
+
 time_series_covid19_deaths_global<-
   time_series_covid19_deaths_global %>%
   unite(Key, Province_State, Country_Region, Date, sep = ".") %>%
   select(Key, Death)
 
-
-time_series_covid19_recovered_global<- 
-  read_csv(url("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv")) %>%
-  rename(Country_Region="Country/Region", Province_State= "Province/State")%>%
-  pivot_longer(-c(Province_State, Country_Region, Lat, Long), names_to= "Date", values_to= "Recovered")
-time_series_covid19_recovered_global<-
-  time_series_covid19_recovered_global %>%
-  unite(Key,Province_State, Country_Region,Date, sep = ".")%>%
-  select(Key, Recovered)
-
-
-
-time_series_long_joined<-
-  full_join(time_series_covid19_confirmed_global, time_series_covid19_death_global, by = c("Key"))
-time_series_long_joined<-
-  full_join(time_series_long_joined, time_series_covid19_recovered_global, by = c("Key"))%>%
+time_series_long_joined_1_ <- full_join(time_series_confirmed_long,
+                                     time_series_deaths_long, by = c("Key"))
+time_series_long_joined <- full_join(time_series_long_joined_1_,
+                                     time_series_recovered_long, by = c("Key")) %>% 
   select(-Key)
 
-time_series_long_joined$Date<- mdy(time_series_long_joined$Date)
+time_series_long_joined$Date <- mdy(time_series_long_joined$Date)
 
-time_series_long_joined_counts<-
-  time_series_long_joined %>%
-  pivot_longer(-c(Province_State, Country_Region, Lat, Long, Date), names_to = "Report_Type", values_to = "Counts")
+
+global_time_series <- time_series_long_joined
 
 
 first_date = min(time_series_long_joined$Date, na.rm = TRUE)
 last_date = max(time_series_long_joined$Date, na.rm = TRUE)
 
-Report_Type = c("Confirmed", "Recovered", "Death")
 
-Countries = time_series_long_joined$Country_Region
+Report_Type = c("Confirmed", "Deaths", "Recovered")
+head(Report_Type)
 
-ui<- fluidPage(
-  titlePanel("COVID-19 Confirmed cases in 5 countries:"),
-  p("Reference: Data for this graph comes from John Hopkins", tags$a("GitHub Respository", href = "https://github.com/CSSEGISandData")
+
+ui <- fluidPage(
+  
+  
+  titlePanel("Exercise 4"),
+  p("Reference: Data for this application are from the Johns Hopkins Center for Systems Science and Engineering",
+    tags$a("GitHub Repository", href="https://github.com/CSSEGISandData")
   ),
   tags$br(),
-  tags$hr(),
+  tags$hr(),  
   
   sidebarLayout(
     sidebarPanel(
-      selectInput("select_type", label = "Report_Type", choices = Report_Type, selected = "Confirmed"),
-      sliderInput("dates", label = "Date reported", min = first_date, max = last_date, value = c(first_date, last_date))
+      
+      selectInput("select_type", 
+                  label = "Report Type", 
+                  choices = Report_Type, selected = "Confirmed"),
+      
+      sliderInput("slider_date", label = "Report Date", min = first_date, 
+                  max = last_date, value = first_date)
     ),
+    
+    
     mainPanel(
       plotOutput("Plot_1_")
-    )))
+    )
+  )
+)
 
-server<- function(input, output){
-  output$Plot1<- renderPlot({
-    pick_country<- time_series_long_joined %>%
-      group_by(Country_Region, Date) %>%
-      summarise_at(c("Confirmed", "Death", "Recovered"), sum) %>%
-      filter(Country_Region %in% c("China", "Japan", "Korea, South", "Iran", "Italy"))
-    ggplot(pick_country, aes_string(x = "Date", y = input$select_type, color = "Country_Region")) +
-      geom_point() +
-      geom_line() +
-      xlim(input$dates) +
-      ggtitle("COVID-19 Confirmed cases in 5 countries:", input$select_type)
+
+server <- function(input, output) {
+  
+  output$Plot1 <- renderPlot({
+    
+    pick_date <- time_series_long_joined %>% 
+      
+      mutate(Country_Region = recode(Country_Region, US = "USA")) %>% 
+      
+      filter(Date == input$slider_date) %>% 
+      group_by(Country_Region) %>% 
+      summarise_at(c("Confirmed", "Deaths", "Recovered"), sum)
+    
+    
+    world <- map_data("world")
+    
+    
+    country_join <- left_join(world, pick_date, by = c("region" = "Country_Region"))
+    
+    
+    ggplot(data = world, mapping = aes(x = long, y = lat, group = group)) + 
+      coord_fixed(1.5) + 
+      
+      geom_polygon(data = country_join, aes_string(fill = input$select_type), color = "black") +
+      scale_fill_gradientn(colours = 
+                             wes_palette("FantasticFox1", 100, type = "continuous"),
+                           trans = "log10") +
+      ggtitle("COVID-19 data", input$select_type)
+    
   })
 }
-
 
 
 # Run the application 
